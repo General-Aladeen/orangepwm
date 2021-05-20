0b1340c887d5d352e06f92081db7d3480b262e0f
General-Aladeen <83897925+General-Aladeen@users.noreply.github.com>
2021-05-20 19:02:28 +0530

    Update example.py

--- example.py
+++ example.py
@@ -1,1 +1,18 @@
 
+from time import sleep
+from pyA20.gpio import gpio
+from pyA20.gpio import port
+from orangepwm import *
+
+
+gpio.init ()
+
+
+led = port.PA21
+pwm = OrangePwm(100, led)
+pwm.start(20)
+
+
+
+
+      
